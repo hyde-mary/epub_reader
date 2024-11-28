8525d5385c164b896095300133873350112b3863
hyde-mary <hydemaryalzbayne@gmail.com>
2024-11-28 18:11:29 +0800

    callback for auth.ts

--- auth.ts
+++ auth.ts
@@ -4,4 +4,34 @@ import GitHub from "next-auth/providers/github";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // here provide the providers, github, google, etc:
   providers: [GitHub],
+  callbacks: {
+    // callback for signin
+    async signIn({ user, account, profile, email, credentials }) {
+      const isAllowedToSignIn = true;
+      if (isAllowedToSignIn) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    //callback for redirects
+    async redirect({ baseUrl }) {
+      return baseUrl;
+    },
+
+    //callback for jwt
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile?.id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
 });

440719947fd1708e4877e9b880770f9ce9820de5
hyde-mary <hydemaryalzbayne@gmail.com>
2024-11-28 20:50:10 +0800

    add loading animation for sign in button

--- components/signin-button.tsx
+++ components/signin-button.tsx
@@ -1,22 +1,34 @@
-import { signIn } from "@/auth";
+"use client";
+import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { Github } from "lucide-react";
+import { Github, Loader } from "lucide-react";
+import { useState } from "react";
 
 export function SignIn() {
+  const [loading, setLoading] = useState(false);
+
+  const handleSignIn = async () => {
+    setLoading(true);
+    await signIn("github");
+  };
+
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("github");
-      }}
+    <Button
+      variant="outline"
+      className="w-full bg-slate-200 text-slate-800"
+      type="submit"
+      onClick={handleSignIn}
+      disabled={loading}
     >
-      <Button
-        variant="outline"
-        className="w-full bg-slate-200 text-slate-800"
-        type="submit"
-      >
-        <Github /> Login with GitHub
-      </Button>
-    </form>
+      {loading ? (
+        <span className="animate-spin h-5 w-5 mr-2">
+          <Loader />
+        </span>
+      ) : (
+        <>
+          <Github className="mr-2" /> Login with GitHub
+        </>
+      )}
+    </Button>
   );
 }

e36f8211d2da6e441aff857e914a0be946072c7e
hyde-mary <hydemaryalzbayne@gmail.com>
2024-11-28 20:23:01 +0800

    add redirect on the auth page when user is authenticated already

--- app/(authentication)/auth/page.tsx
+++ app/(authentication)/auth/page.tsx
@@ -1,7 +1,16 @@
+import { auth } from "@/auth";
 import { LoginForm } from "@/components/login-form";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+
+  // we also check here if the user is logged in, if so, just redirect to /
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <div className="flex h-screen w-full items-center justify-center px-4 bg-slate-950">
       <LoginForm />

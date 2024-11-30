36a9ccf0456e7f43421b18079dac0a2dee9a3c12
hyde-mary <hydemaryalzbayne@gmail.com>
2024-11-30 16:52:17 +0800

    add homepage view when user has no books

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import Navbar from "@/components/nav-bar";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -9,9 +10,45 @@ export default async function Home() {
     redirect("/auth");
   }
 
+  const books = [];
+
   return (
     <>
       <Navbar username={session?.user?.name} image={session?.user?.image} />
+      {books.length > 0 ? (
+        // if books.length > 0, we display it, else we render something that prompts the user to add something
+        <section className="h-screen flex items-center justify-center">
+          <h1 className="text-3xl">Display Books</h1>
+        </section>
+      ) : (
+        <section className="h-screen flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center max-w-full mx-auto text-center">
+            <div className="mb-8">
+              <Image
+                src="/homepage_astronaut.png"
+                alt="A photo of an astronaut found in the homepage"
+                width={500}
+                height={500}
+                className="mx-auto"
+              />
+            </div>
+            <h1 className="xl:text-7xl text-4xl font-extrabold text-slate-950 mb-10 underline">
+              Your Library is Currently Empty.
+            </h1>
+            <p className="text-2xl text-gray-600">
+              It seems like you do not have any books yet. Let us get started by
+              adding your first book!
+            </p>
+            <p className="text-gray-500 mt-5 italic">
+              &quot;A book is a dream you hold in your hands.&quot; – Neil
+              Gaiman
+            </p>
+            <p className="text-xl mt-10 mb-5 font-extrabold">
+              Click the + icon at the top to start adding your books.
+            </p>
+          </div>
+        </section>
+      )}
     </>
   );
 }

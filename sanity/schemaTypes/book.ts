2fd45d67fd9556418cd4a45b6e44472db8cbb6d4
hyde-mary <hydemaryalzbayne@gmail.com>
2024-11-30 21:25:06 +0800

    add: book schema

---
+++ sanity/schemaTypes/book.ts
@@ -0,0 +1,42 @@
+import { Book } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+export const book = defineType({
+  name: "book",
+  title: "Book",
+  type: "document",
+  icon: Book,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "author",
+      type: "string",
+    }),
+    defineField({
+      name: "user",
+      type: "reference",
+      to: { type: "user" },
+    }),
+    defineField({
+      name: "cover",
+      type: "url",
+    }),
+    defineField({
+      name: "file_id",
+      type: "string",
+    }),
+    {
+      name: "file",
+      title: "File",
+      type: "file",
+    },
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+});

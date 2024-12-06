4df459ebf5ddff1504e309370c3f3f8b36dc7ccd
hyde-mary <hydemaryalzbayne@gmail.com>
2024-12-06 18:10:47 +0800

    feature: dock with semi functioning buttons

--- components/book-reader.tsx
+++ components/book-reader.tsx
@@ -1,200 +1,52 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
-import { ReactReader, ReactReaderStyle } from "react-reader";
-import type { Rendition } from "epubjs";
-import Link from "next/link";
-import { Home, MinusCircle, PlusCircle } from "lucide-react";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import useLocalStorageState from "use-local-storage-state";
+import React, { useState } from "react";
+import DockReader from "./dock-reader";
+
+interface Book {
+  _id: string;
+  title: string;
+  user: string;
+  author: string;
+  image_url: string;
+  file_id: string;
+  _createdAt: string;
+}
+
+interface File {
+  _id: string;
+  originalFilename: string;
+  url: string;
+  size: number;
+  extension: string;
+  _createdAt: string;
+}
 
 interface BookReaderProps {
-  file: { url: string };
-  title: string;
+  book: Book;
+  file: File;
 }
 
-type ITheme = "light" | "dark";
-
-function updateTheme(rendition: Rendition, theme: ITheme) {
-  const themes = rendition.themes;
-  switch (theme) {
-    case "dark": {
-      themes.override("color", "#fff");
-      themes.override("background", "#000");
-      break;
-    }
-    case "light": {
-      themes.override("color", "#000");
-      themes.override("background", "#fff");
-      break;
-    }
-  }
-}
-
-const BookReader = ({ file, title }: BookReaderProps) => {
-  const [location, setLocation] = useLocalStorageState<string | number>(
-    "persist-location",
-    {
-      defaultValue: 0,
-    }
-  );
-  const [fontSize, setFontSize] = useLocalStorageState<number>(
-    "persist-font-size", // Save font size in localStorage
-    {
-      defaultValue: 16, // Default font size
-    }
-  );
-
-  const rendition = useRef<Rendition | undefined>(undefined);
-  const [theme, setTheme] = useState<ITheme>("dark");
-
-  useEffect(() => {
-    if (rendition.current) {
-      updateTheme(rendition.current, theme);
-      // Apply the font size dynamically
-      rendition.current.themes.fontSize(`${fontSize}px`);
-    }
-  }, [theme, fontSize]); // Update when theme or fontSize changes
-
-  const increaseFontSize = () => {
-    setFontSize((prevSize) => Math.min(prevSize + 2, 24)); // Limit max size to 24px
-  };
-
-  const decreaseFontSize = () => {
-    setFontSize((prevSize) => Math.max(prevSize - 2, 12)); // Limit min size to 12px
-  };
-
+const BookReader = ({ book, file }: BookReaderProps) => {
+  const [fontSize, setFontSize] = useState<number>(16);
+  const file_url = file.url;
   return (
-    <div className={`h-screen ${theme === "dark" ? "bg-[#000]" : "bg-[#fff]"}`}>
-      {/* Navbar for other stuff and buttons */}
-      <div className="w-full h-[54px] mx-auto px-2 sm:px-3 lg:px-4">
-        <div className="relative flex items-center h-[54px]">
-          <div className="flex items-center absolute left-0">
-            <Link className="z-50" href={"/"}>
-              <Home
-                className={`w-6 h-6 ${theme === "dark" ? "text-[#fff]" : "text-[#000]"}`}
-              />
-            </Link>
-          </div>
-          <div className="absolute inset-0 flex justify-center items-center">
-            <MinusCircle
-              onClick={decreaseFontSize}
-              className={`w-6 h-6 ${theme === "dark" ? "text-[#fff]" : "text-[#000]"} mr-4`}
-            />
-            <p
-              className={`${theme === "dark" ? "text-[#fff]" : "text-[#000]"} font-semibold text-lg`}
-            >
-              Font Size
-            </p>
-            <PlusCircle
-              onClick={increaseFontSize}
-              className={`w-6 h-6 ${theme === "dark" ? "text-[#fff]" : "text-[#000]"} ml-4`}
-            />
-          </div>
-          <div className="absolute right-0">
-            <Label
-              htmlFor="dark-mode"
-              className={`${theme === "dark" ? "text-[#fff]" : "text-[#000]"} font-semibold text-lg`}
-            >
-              Dark Mode
-              <Switch
-                id="dark-mode"
-                className="ml-4"
-                checked={theme === "dark"} // Bind to the theme state
-                onCheckedChange={() =>
-                  setTheme(theme === "dark" ? "light" : "dark")
-                } // Toggle the theme on change
-              />
-            </Label>
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col h-screen overflow-hidden">
+      <section className="flex-grow-[8] bg-slate-100"></section>
 
-      {/* ReactReader for the book */}
-      <div className="h-[calc(100vh-54px)]">
-        <ReactReader
-          url={file.url}
-          title={title}
-          location={location}
-          locationChanged={(loc: string) => setLocation(loc)}
-          readerStyles={
-            theme === "dark" ? darkReaderTheme() : lightReaderTheme()
-          }
-          getRendition={(_rendition) => {
-            updateTheme(_rendition, theme);
-            rendition.current = _rendition;
-            // Apply font size to the rendition
-            rendition.current.themes.fontSize(`${fontSize}px`);
-          }}
-        />
+      <div className="relative group">
+        <div
+          className={`absolute left-1/2 bottom-[-45px] transform -translate-x-1/2 bg-slate-200 border border-slate-400 shadow-2xl rounded-xl p-6 max-w-4xl w-3/4 transition-all duration-300 ease-in-out group-hover:bottom-2`}
+        >
+          <DockReader
+            fontSize={fontSize}
+            setFontSize={setFontSize}
+            file_url={file_url}
+          />
+        </div>
       </div>
     </div>
   );
 };
-
-const lightReaderTheme = () => ({
-  ...ReactReaderStyle,
-  readerArea: {
-    ...ReactReaderStyle.readerArea,
-    transition: undefined,
-  },
-  titleArea: {
-    ...ReactReaderStyle.titleArea,
-    color: "black",
-    fontSize: "24px",
-    fontWeight: "bold",
-    textDecoration: "underline",
-  },
-  tocArea: {
-    ...ReactReaderStyle.tocArea,
-    fontSize: "16px",
-  },
-  tocButton: {
-    ...ReactReaderStyle.tocButton,
-    color: "black",
-  },
-});
-
-const darkReaderTheme = () => ({
-  ...ReactReaderStyle,
-  arrow: {
-    ...ReactReaderStyle.arrow,
-    color: "white",
-  },
-  arrowHover: {
-    ...ReactReaderStyle.arrowHover,
-    color: "#ccc",
-  },
-  readerArea: {
-    ...ReactReaderStyle.readerArea,
-    backgroundColor: "#000",
-    transition: undefined,
-  },
-  titleArea: {
-    ...ReactReaderStyle.titleArea,
-    color: "#ccc",
-    fontSize: "24px",
-    fontWeight: "bold",
-    textDecoration: "underline",
-  },
-  tocArea: {
-    ...ReactReaderStyle.tocArea,
-    background: "#111",
-    fontSize: "16px",
-  },
-  tocButtonExpanded: {
-    ...ReactReaderStyle.tocButtonExpanded,
-    background: "#222",
-  },
-  tocButtonBar: {
-    ...ReactReaderStyle.tocButtonBar,
-    background: "#fff",
-  },
-  tocButton: {
-    ...ReactReaderStyle.tocButton,
-    color: "white",
-  },
-});
 
 export default BookReader;
---
+++ components/dock-reader.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Download, HomeIcon, MinusCircle, PlusCircle } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+interface DockReaderProps {
+  fontSize: number;
+  setFontSize: (size: number) => void;
+  file_url: string;
+}
+
+const DockReader = ({ fontSize, setFontSize, file_url }: DockReaderProps) => {
+  const increaseFontSize = () => {
+    if (fontSize < 24) {
+      setFontSize(fontSize + 2);
+    }
+    console.log(fontSize);
+  };
+
+  const decreaseFontSize = () => {
+    if (fontSize > 12) {
+      setFontSize(fontSize - 2);
+    }
+    console.log(fontSize);
+  };
+
+  return (
+    <div className="w-full mx-auto px-2 sm:px-3 lg:px-4 p-1 pt-2 pb-2">
+      <div className="relative flex items-center">
+        <div className="flex items-center absolute left-0 text-black">
+          <Link href="/">
+            <HomeIcon className="w-6 h-6" />
+          </Link>
+        </div>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <MinusCircle className="w-6 h-6 mr-2" onClick={decreaseFontSize} />
+          <p className="font-semibold text-lg">Font Size</p>
+          <PlusCircle className="w-6 h-6 ml-2" onClick={increaseFontSize} />
+        </div>
+        <div className="absolute right-0 flex">
+          <a href={file_url} download className="w-6 h-6 cursor-pointer">
+            <Download className="w-6 h-6" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DockReader;
